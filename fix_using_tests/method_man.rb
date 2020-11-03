0db6e3e9570e55f81d144e3f54374f7aba162888
Misel Evtimov <misel.evtimov@gmail.com>
2020-11-03 01:06:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,8 +1,8 @@
-require 'pry'
+
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 end
-
+require 'pry'
 def play_game(player2)
   binding.pry
   "#{player1} is better than #{player2}"
