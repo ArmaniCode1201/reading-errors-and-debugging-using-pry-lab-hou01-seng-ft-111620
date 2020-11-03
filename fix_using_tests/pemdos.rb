cedae45e590ec263b1be2626675296499a8b5551
Misel Evtimov <misel.evtimov@gmail.com>
2020-11-03 16:35:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 
 def snake_it_up(string)
   if string[0] = "s"
-    #10 * "s" + string
-    10.times do "s" + string
+    10 * "s" + string
+    #10.times do "s" + string
   else
     string
   end
