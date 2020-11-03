9c9faa208c8e60d125797463a1f47da1bcb1ee7c
Misel Evtimov <misel.evtimov@gmail.com>
2020-11-03 16:07:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -9,6 +9,7 @@ def state_log(star_date)
 end
 
 def engage
-  date = generate_star_date
+  binding.pry
   puts state_log(date)
+  date = generate_star_date
 end
