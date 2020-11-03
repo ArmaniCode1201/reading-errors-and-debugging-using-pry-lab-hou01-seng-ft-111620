2232d7ba898204a261d7f21b6e80891d9b78a641
Misel Evtimov <misel.evtimov@gmail.com>
2020-11-03 16:50:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
-    #10.times {puts "s" + string}
+    #10 * "s" + string
+    10.times {|i| puts string}
   else
     string
   end
