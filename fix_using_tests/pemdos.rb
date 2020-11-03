06efb151fb2449a2a80f442f2aed7086b374dda7
Misel Evtimov <misel.evtimov@gmail.com>
2020-11-03 15:58:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,6 @@
 # don't forget to add: require 'pry'
-require 'pry'
 def snake_it_up(string)
   if string[0] = "s"
-    "s" = "s".to_i
     10 * "s" + string
     #10.times {"s" print string}
   else
