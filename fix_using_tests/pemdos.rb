c64d270833505d4d2beb8c08bf55dc236d1b55c7
Misel Evtimov <misel.evtimov@gmail.com>
2020-11-03 16:58:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 require 'pry'
 def snake_it_up(string)
   binding.pry
-  if string[0] = "s"
-      10 * "s" + string
-    #10.times {"s"} + string
+  if string[0] = "s"   
+    #10 * "s" + string
+    10.times {|i|} + string
   else
     string
   end
